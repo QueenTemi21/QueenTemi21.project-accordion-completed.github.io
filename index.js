239f74bdc9a161a1355597fc2209ecd34f21cb01
QueenTemi21 <oladapotope482@gmail.com>
2023-09-20 11:58:28 -0700

    My Accordion project is completed
    i just finished my accordion project

---
+++ index.js
@@ -0,0 +1,31 @@
+const questions = document.querySelectorAll(".question")
+
+//questions.forEach((question) => {
+   // question.addEventListener("click", () => {
+        //question.classList.toggle("open")
+    //})
+//}) 
+//questions.forEach((question) => {
+    //question.addEventListener("click", () => {
+        //if (question.classList.contains("open")) {
+           // question.classList.remove("open")
+       // } else{
+            //question.classList.add()
+       // }
+    //})
+//})
+
+questions.forEach((question) => {
+    question.addEventListener("click", () => {
+        if (question.classList.contains("open")) {
+            question.classList.remove("open")
+        } else{
+            const questionsIsOpen = document.querySelectorAll(".open")
+questionsIsOpen.forEach((quest) => {
+    quest.classList.remove("open")
+})
+
+            question.classList.add("open")
+        }
+    })
+})
